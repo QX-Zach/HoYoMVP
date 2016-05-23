205d1af3a55ff042deaa686384d41abb290115f4
mengdongya <mdy_710@126.com>
2016-05-23 10:35:04 +0800

    工具

--- app/src/main/java/com/example/ozner/hoyomvp/Bean/Utils.java
+++ app/src/main/java/com/example/ozner/hoyomvp/Bean/Utils.java
@@ -4,4 +4,8 @@ package com.example.ozner.hoyomvp.Bean;
  * Created by cecha on 2016/5/23.
  */
 public class Utils {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
